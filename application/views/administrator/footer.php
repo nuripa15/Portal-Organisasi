6e8b606fb805a3a8c818497d3bdf8ce73b6aa400
unknown <riffaalbantani@gmail.com>
2022-04-19 01:22:25 -0700

    first commit

---
+++ application/views/administrator/footer.php
@@ -0,0 +1,2 @@
+
+<strong>Copyright &copy; <?php echo date('Y'); ?> <a target='_BLANK' href="http://www.komsusid.com"> Website KOMSUS_ID</a>.</strong> All rights reserved. 
---
+++ application/views/lokomedia_sekolah/footer.php
@@ -0,0 +1,7 @@
+
+<?php 
+echo "<div class='wrapper' style='padding-top:0px;'><br>
+	<center>Komsusid © ".date('Y')." All Right Reserved</center>
+</div>";
+
+
